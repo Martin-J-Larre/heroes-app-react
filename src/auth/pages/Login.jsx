d6283b69d66d5081368b7f3be31bbbc994430b94
Martin-J-Larre <martinjlarre@gmail.com>
2023-04-19 01:02:34 -0400

    #5 List heroes by publisher and helper getHeroesByPublisher added

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const onLogin = () => {
+    navigate("/", {
+      replace: true,
+    });
+  };
+  return (
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+
+      <button className="btn btn-primary" onClick={onLogin}>
+        Login
+      </button>
+    </div>
+  );
 };
