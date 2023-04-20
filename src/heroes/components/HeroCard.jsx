c85c26f31ad8b74594a80931f94f39d9e215e31f
Martin-J-Larre <martinjlarre@gmail.com>
2023-04-19 23:27:45 -0400

    #6 Add prop-types and HeroCard component

---
+++ src/heroes/components/HeroCard.jsx
@@ -0,0 +1,44 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+export const HeroCard = ({
+  id,
+  superhero,
+  publisher,
+  alter_ego,
+  first_appearance,
+  characters,
+}) => {
+  const heroImg = `./assets/heroes/heroes/${id}.jpg`;
+  return (
+    <div className="col">
+      <div className="card">
+        <div className="row no-gutter">
+          <div className="col-4">
+            <img src={heroImg} className="card-img" alt={superhero} />
+          </div>
+          <div className="col-8">
+            <div className="card-body">
+              <h5 className="card-title">{superhero}</h5>
+              <p className="card-text">{alter_ego}</p>
+              {alter_ego !== characters && <p>{characters}</p>}
+              <p className="card-text">
+                <small className="text-muted">{first_appearance}</small>
+              </p>
+              <Link to={`/hero/${id}`}>more...</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+HeroCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  superhero: PropTypes.string.isRequired,
+  publisher: PropTypes.string.isRequired,
+  alter_ego: PropTypes.string.isRequired,
+  first_appearance: PropTypes.string.isRequired,
+  characters: PropTypes.string.isRequired,
+};
